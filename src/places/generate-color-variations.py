8a797f024f250ec3ae067ce356fcc649f240826a
anaximeno <anaximenobrito@gmail.com>
2024-05-03 01:28:27 -0100

    style: Update the system folder style and background colors

--- src/places/generate-color-variations.py
+++ src/places/generate-color-variations.py
@@ -4,23 +4,22 @@ import os
 # This script uses src.svg and generates svg files for each defined color
 # It uses the following color table to do so:
 
-SRC = {"folder":"eeca8f","backfolder":"c89e6b","paper":"e8e8e8","emblem":"575757", "file-manager-back": "ce9f52"}
-VARIANTS = []
+SRC = {"folder":"eeca8f","backfolder":"c89e6b","paper":"e8e8e8","emblem":"575757", "system-file-manager-back": "eab768"}
 
-# Blue
-VARIANTS.append({"name":"Blue","folder":"5294e2","backfolder":"4877b1","paper":"e4e4e4","emblem":"1d344f", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Aqua","folder":"57b8ec","backfolder":"147eb8","paper":"e4e4e4","emblem":"106796", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Teal","folder":"45abb7","backfolder":"35818a","paper":"e4e4e4","emblem":"eaeaea", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Green","folder":"50c16f","backfolder":"1f953f","paper":"e4e4e4","emblem":"2f3e1f", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Sand","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","emblem":"4a4a4a", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Grey","folder":"aaaaaa","backfolder":"727272","paper":"ffffff","emblem":"4f4f4f", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Orange","folder":"ff804f","backfolder":"f2632a","paper":"e4e4e4","emblem":"4a4a4a", "file-manager-back": "fee698"})
-VARIANTS.append({"name": "Red","folder":"f54f54","backfolder":"ca0f14","paper":"e4e4e4","emblem":"4d1c1c", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Pink","folder":"f26a9a","backfolder":"cf2f67","paper":"e4e4e4","emblem":"542233", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Purple","folder":"a27ae4","backfolder":"7240c3","paper":"e4e4e4","emblem":"2c1e44", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Cyan","folder":"00bcd4","backfolder":"0096aa","paper":"e4e4e4","emblem":"00424a", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Navy","folder":"b8d8eb","backfolder":"004988","paper":"f4f4f4","emblem":"081f2d", "file-manager-back": "fee698"})
-VARIANTS.append({"name":"Yaru","folder":"676767","backfolder":"973552","paper":"ff7446","emblem":"e4e4e4", "file-manager-back": "fee698"})
+VARIANTS = []
+VARIANTS.append({"name":"Blue","folder":"5294e2","backfolder":"4877b1","paper":"e4e4e4","emblem":"1d344f", "system-file-manager-back": "3f4756"})
+VARIANTS.append({"name":"Aqua","folder":"57b8ec","backfolder":"147eb8","paper":"e4e4e4","emblem":"106796", "system-file-manager-back": "374955"})
+VARIANTS.append({"name":"Teal","folder":"45abb7","backfolder":"35818a","paper":"e4e4e4","emblem":"eaeaea", "system-file-manager-back": "324b4e"})
+VARIANTS.append({"name":"Green","folder":"50c16f","backfolder":"1f953f","paper":"e4e4e4","emblem":"2f3e1f", "system-file-manager-back": "3c4a3e"})
+VARIANTS.append({"name":"Sand","folder":"f9c470","backfolder":"e0a84f","paper":"e4e4e4","emblem":"4a4a4a", "system-file-manager-back": "4f4538"})
+VARIANTS.append({"name":"Grey","folder":"aaaaaa","backfolder":"727272","paper":"ffffff","emblem":"4f4f4f", "system-file-manager-back": "474554"})
+VARIANTS.append({"name":"Orange","folder":"ff804f","backfolder":"f2632a","paper":"e4e4e4","emblem":"4a4a4a", "system-file-manager-back": "55433c"})
+VARIANTS.append({"name": "Red","folder":"f54f54","backfolder":"ca0f14","paper":"e4e4e4","emblem":"4d1c1c", "system-file-manager-back": "574240"})
+VARIANTS.append({"name":"Pink","folder":"f26a9a","backfolder":"cf2f67","paper":"e4e4e4","emblem":"542233", "system-file-manager-back": "564147"})
+VARIANTS.append({"name":"Purple","folder":"a27ae4","backfolder":"7240c3","paper":"e4e4e4","emblem":"2c1e44", "system-file-manager-back": "4b4453"})
+VARIANTS.append({"name":"Cyan","folder":"00bcd4","backfolder":"0096aa","paper":"e4e4e4","emblem":"00424a", "system-file-manager-back": "324b50"})
+VARIANTS.append({"name":"Navy","folder":"b8d8eb","backfolder":"004988","paper":"f4f4f4","emblem":"081f2d", "system-file-manager-back": "364954"})
+VARIANTS.append({"name":"Yaru","folder":"676767","backfolder":"973552","paper":"ff7446","emblem":"e4e4e4", "system-file-manager-back": "474554"})
 
 for filename in os.listdir("."):
     if filename.endswith(".svg"):
